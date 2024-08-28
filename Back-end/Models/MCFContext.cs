1f91f44468aca24f1c3283962fbb24e2dc31507c
Ginanjar <ganjar56@gmail.com>
2024-08-28 16:54:19 +0700

    Commit Default

---
+++ Back-end/Models/MCFContext.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Back_end.Models
+{
+    public partial class MCFContext : DbContext
+    {
+        public MCFContext()
+        {
+        }
+
+        public MCFContext(DbContextOptions<MCFContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<MsStorageLocation> MsStorageLocations { get; set; } = null!;
+        public virtual DbSet<MsUser> MsUsers { get; set; } = null!;
+        public virtual DbSet<TrBpkb> TrBpkbs { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MsStorageLocation>(entity =>
+            {
+                entity.HasKey(e => e.LocationId);
+
+                entity.ToTable("ms_storage_location");
+
+                entity.Property(e => e.LocationId)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("location_id");
+
+                entity.Property(e => e.LocationName)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("location_name");
+            });
+
+            modelBuilder.Entity<MsUser>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+
+                entity.ToTable("ms_user");
+
+                entity.Property(e => e.UserId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("user_id");
+
+                entity.Property(e => e.IsActive).HasColumnName("is_active");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("user_name");
+            });
+
+            modelBuilder.Entity<TrBpkb>(entity =>
+            {
+                entity.HasKey(e => e.AgreementNumber);
+
+                entity.ToTable("tr_bpkb");
+
+                entity.Property(e => e.AgreementNumber)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("agreement_number");
+
+                entity.Property(e => e.BpkbDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("bpkb_date");
+
+                entity.Property(e => e.BpkbDateIn)
+                    .HasColumnType("datetime")
+                    .HasColumnName("bpkb_date_in");
+
+                entity.Property(e => e.BpkbNo)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("bpkb_no");
+
+                entity.Property(e => e.BranchId)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("branch_id");
+
+                entity.Property(e => e.CreatedBy)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("created_by");
+
+                entity.Property(e => e.CreatedOn)
+                    .HasColumnType("datetime")
+                    .HasColumnName("created_on");
+
+                entity.Property(e => e.FakturDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("faktur_date");
+
+                entity.Property(e => e.FakturNo)
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("faktur_no");
+
+                entity.Property(e => e.LastUpdatedBy)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("last_updated_by");
+
+                entity.Property(e => e.LastUpdatedOn)
+                    .HasColumnType("datetime")
+                    .HasColumnName("last_updated_on");
+
+                entity.Property(e => e.LocationId)
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .HasColumnName("location_id");
+
+                entity.Property(e => e.PoliceNo)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("police_no");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Back-end/Models/MsStorageLocation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Back_end.Models
+{
+    public partial class MsStorageLocation
+    {
+        public string LocationId { get; set; } = null!;
+        public string? LocationName { get; set; }
+    }
+}
